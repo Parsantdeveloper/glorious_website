078483bf022781894664ca0bb8580930c0eee8d7
“[Prashant <parshantbhusal1928@gmail.com>
2026-03-15 15:00:57 +0545

    added dynamic to product/layout.tsx

--- dashboard-frontend/app/dashboard/product/layout.tsx
+++ dashboard-frontend/app/dashboard/product/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic"
+
 import FilterMetaProvider  from '@/components/dashboard/product/FilterMetaProvider'
 export default async function ProductLayout({
   children,
